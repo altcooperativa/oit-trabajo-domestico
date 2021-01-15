f0f6785a3ff263ba5e0b992eb6689d3cb978805b
Vittorio Taddeo <58092735+DoubleT-DoubleD@users.noreply.github.com>
2021-01-14 21:21:21 -0300

    Actualización

--- js/script.js
+++ js/script.js
@@ -1,36 +1,7 @@
 window.addEventListener( 'load', function() {
-	var hamburgerButtons = document.getElementsByClassName('hamburger-button');
+	var hamburgerButton = document.getElementById( 'hamburger-button');
 
-	for ( var i = 0; i < hamburgerButtons.length; i++ ) {
-		hamburgerButtons[ i ].addEventListener( 'click', function() {
-			if ( ! document.body.classList.contains( 'hamburger-menu-open' ) &&
-			! document.body.classList.contains( 'hamburger-menu-closed' ) ) {
-				document.body.classList.add( 'hamburger-menu-open' );
-			} else {
-				document.body.classList.toggle( 'hamburger-menu-open' );
-				document.body.classList.toggle( 'hamburger-menu-closed' );
-			}
-		} );
-	}
-
-	var expandableImages = document.getElementsByClassName('img-expandable');
-
-	for ( var i = 0; i < expandableImages.length; i++ ) {
-		expandableImages[ i ].addEventListener( 'click', function( e ) {
-			let expanded = document.getElementsByClassName('img-expanded');
-
-			if ( ! expanded.length ) {
-				this.classList.add('img-expanded');
-				e.stopPropagation();
-			}
-		} );
-	}
-
-	document.addEventListener( 'click', function() {
-		let expanded = document.getElementsByClassName('img-expanded');
-
-		for (let index = 0; index < expanded.length; index++) {
-			expanded[ index ].classList.remove('img-expanded');
-		}
+	hamburgerButton.addEventListener( 'click', function() {
+		document.body.classList.toggle( 'hamburger-menu-active' );
 	} );
 } );
